721ccc492ce7d0b18d2a48cee77c0a67546d3645
HuseyinAkkoc <156607663+HuseyinAkkoc@users.noreply.github.com>
2025-10-14 21:42:07 -0400

    Health bar added. Health bar Ui fixed. Live system  fixed.

---
+++ Assets/ScriptableObject/Scripts/Spawner.cs
@@ -0,0 +1,136 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public static event Action<int> OnWaveChanged;
+
+    private float _spawnTimer;
+    private float _spawnCounter;
+    private int _enemiesRemoved;
+
+
+    private float _timeBetweenWaves = 2f;
+    private float _waveCooldown;
+    private bool _isBetweenWaves;
+
+
+
+    [SerializeField] private ObjectPooler zombiePool;
+    [SerializeField] private ObjectPooler batPool;
+    [SerializeField] private ObjectPooler golemPool;
+
+
+    private WaveData CurrentWave => waves[_currentWaveindex];
+    [SerializeField] private WaveData[] waves;
+    private int _currentWaveindex = 0;
+    private int _waveCounter = 0;
+  
+
+
+    private Dictionary<EnemyType, ObjectPooler> _poolDictionary;
+
+    
+
+    private void Awake()
+    {
+        _poolDictionary = new Dictionary<EnemyType, ObjectPooler>()
+        {
+            { EnemyType.Zombie, zombiePool },
+            { EnemyType.Bat, batPool },
+            { EnemyType.Golem, golemPool },
+
+        };
+    }
+
+    private void Start()
+    {
+        OnWaveChanged?.Invoke(_waveCounter);
+    }
+
+    private void OnEnable()
+    {
+        Enemy.OnEnemyReachedEnd += HandleEnemyReachedEnd;
+        Enemy.OnEnemyDestroyed += HandleEnemyDestroyed;
+    }
+
+
+    private void OnDisable()
+    {
+        Enemy.OnEnemyReachedEnd -= HandleEnemyReachedEnd; 
+        Enemy.OnEnemyDestroyed -= HandleEnemyDestroyed;
+    }
+    private void Update()
+    {
+
+
+        if(_isBetweenWaves)
+        {
+            _waveCooldown -= Time.deltaTime;
+            if(_waveCooldown <= 0f )
+            {
+                _currentWaveindex = (_currentWaveindex + 1) % waves.Length;
+                _waveCounter++;
+                OnWaveChanged?.Invoke( _waveCounter );
+                _spawnCounter = 0;
+                _enemiesRemoved = 0;
+                _spawnTimer = 0f;
+                _isBetweenWaves = false;
+            }
+        }
+        else
+        {
+            _spawnTimer -= Time.deltaTime;
+
+            if (_spawnTimer <= 0 && _spawnCounter < CurrentWave.enemiesPerWave)
+            {
+                _spawnTimer = CurrentWave.spawnInterval;
+                SpawnEnemy();
+                _spawnCounter++;
+
+            }
+            else if (_spawnCounter >= CurrentWave.enemiesPerWave && _enemiesRemoved >= CurrentWave.enemiesPerWave)
+            {
+               
+                _isBetweenWaves = true;
+                _waveCooldown= _timeBetweenWaves;
+            }
+        }
+        
+            
+    }
+
+      
+    private void SpawnEnemy()
+    {
+
+        if(_poolDictionary.TryGetValue(CurrentWave.enemyType, out var pool))
+        {
+            GameObject spawnedObject = pool.GetPooledObject();
+            spawnedObject.transform.position = transform.position;
+            spawnedObject.SetActive(true);
+
+            float healthMultiplier = 1f + (+_waveCounter * 0.1f);
+            Enemy enemy = spawnedObject.GetComponent<Enemy>();
+            enemy.Initialize(healthMultiplier);
+        }
+      
+
+
+    }
+
+
+
+    private void HandleEnemyReachedEnd(EnemyData data)
+    {
+        _enemiesRemoved++;
+    }
+
+
+    private void HandleEnemyDestroyed( Enemy enemy)
+    {
+        _enemiesRemoved++;
+    }
+}
